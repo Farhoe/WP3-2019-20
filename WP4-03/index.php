5a25fa1753e9940768848e9cc32de270896cc2ba
Matthew <37181565+Farhoe@users.noreply.github.com>
2019-12-05 17:54:24 +0100

    Delame jakoze databaze na dluzniky :DDD
    jj

---
+++ WP4-03/index.php
@@ -0,0 +1,35 @@
+<?php 
+require_once 'classes' . DIRECTORY_SEPARATOR . 'Database.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Šrouby od matky</title>
+</head>
+<body>
+<?php 
+$sqlDebitors = "SELECT * FROM Debitors;"; 
+$resultDebitors = Database::query($sqlDebitors); ?>
+    <table class="table table-striped">
+<?php
+while ($row = $resultDebitors->fetch_assoc()) {?>
+<tr>
+    <td><?= $row['firstname'];?></td>
+    <td><?= $row['lastname'];?></td>
+    <td><?= $row['id_debitors'];?></td>
+    <td><?= $row['birth_date'];?></td>
+    <td><?= $row['day_of_death'];?></td>
+</tr>
+<?php }?>
+</table>
+
+
+</body>
+</html>
